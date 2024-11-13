1bcf8709259900fad478b5d2819879d6c0a5bc33
Brenn <brennaldrich@gmail.com>
2024-11-13 09:32:20 -0500

    Game interface setup

--- src/main/java/rpg/framework/Game.java
+++ src/main/java/rpg/framework/Game.java
@@ -2,6 +2,13 @@ package rpg.framework;
 
 public interface Game {
   // === Accessor methods ===================================
-    
+
+    public void getPosition();
+
+
   // === Mutator methods ======================================
+
+    public boolean moveRoom ();
+
+    public void endTurn();
 }
--- src/main/java/rpg/standard/GameImpl.java
+++ src/main/java/rpg/standard/GameImpl.java
@@ -4,4 +4,15 @@ import rpg.framework.*;
 
 public class GameImpl implements Game {
 
+    public void getPosition() {
+
+    }
+
+    public boolean moveRoom() {
+        return false;
+    }
+
+    public void endTurn() {
+
+    }
 }
